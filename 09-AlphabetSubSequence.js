940676dea944da1d850723a2ecc074044ec99b12
Amaury <0clues@protonmail.com>
2021-07-13 23:32:54 -0400

    09-exercise-009

---
+++ 09-AlphabetSubSequence.js
@@ -0,0 +1,36 @@
+/** 
+    Check whether the given string is a subsequence of the plaintext alphabet.
+
+    Example
+    - For s = "effg" or s = "cdce", the output should be
+    alphabetSubsequence(s) = false
+
+    - For s = "ace" or s = "bxz", the output should be
+    alphabetSubsequence(s) = true.
+
+    Hints
+    -   size property
+    -   charCodeAt()
+    -   split()
+
+    Input/Output
+    - **[execution time limit] 5 seconds (ts)**
+    - **[input] string s**
+
+    *Guaranteed constraints:*
+    2 ≤ s.length ≤ 15.
+
+    [output] boolean
+    true if the given string is a subsequence of the alphabet, false otherwise.
+*/
+
+function alphabetSubsequence(string) {
+
+}
+
+console.log(alphabetSubsequence('zab'))
+console.log(alphabetSubsequence('effg'))
+console.log(alphabetSubsequence('cdce'))
+console.log(alphabetSubsequence('ace'))
+console.log(alphabetSubsequence('bxz'))
+ 
