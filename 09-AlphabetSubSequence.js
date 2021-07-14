3dab5f2a1b2be187cbf54f66d94e34f6bafd8589
Amaury <0clues@protonmail.com>
2021-07-13 23:33:48 -0400

    09-exercise-009

--- 09-AlphabetSubSequence.js
+++ 09-AlphabetSubSequence.js
@@ -24,8 +24,25 @@
     true if the given string is a subsequence of the alphabet, false otherwise.
 */
 
-function alphabetSubsequence(string) {
+function alphabetSubsequence(strings) {
+    const chars = strings.split('');
+    const charValues = [];
 
+    chars.forEach(char => {
+        charValues.push(char.charCodeAt(0));
+    })
+
+    if (new Set(charValues).size !== charValues.length) {
+        return false;
+    }
+
+    for (let i = 0; i < charValues.length - 1; i++) {
+        if (charValues[i] >= charValues[i + 1]) {
+            return false;
+        }
+        
+    }
+    return true;
 }
 
 console.log(alphabetSubsequence('zab'))
