d59fdcf323d877aefe2b89cb253df71deaa27aa1
Amaury <0clues@protonmail.com>
2021-07-17 12:06:53 -0400

    exercise-015

---
+++ 15-arrayMaxConsecutiveSum.js
@@ -0,0 +1,55 @@
+/**
+    Given array of integers, find the maximal possible sum of some of its k consecutive elements.
+
+    Example
+    For inputArray = [2, 3, 5, 1, 6] and k = 2, the output should be
+    arrayMaxConsecutiveSum(inputArray, k) = 8.
+    All possible sums of 2 consecutive elements are:
+    -   2 + 3 = 5;
+    -   3 + 5 = 8;
+    -   5 + 1 = 6;
+    -   1 + 6 = 7.
+    Thus, the answer is 8
+
+
+    Input/Output
+    - **[execution time limit] 4 seconds (js)**
+    - **[input] array.integer inputArray** Array of positive integers.
+
+    Guaranteed constraints:
+    3 ≤ inputArray.length ≤ 105,
+    1 ≤ inputArray[i] ≤ 1000.
+    
+    - [input] integer k
+    An integer (not greater than the length of inputArray).
+    
+    Guaranteed constraints:
+    1 ≤ k ≤ inputArray.length.
+    
+    - [output] integer
+    The maximal possible sum.
+*/
+
+function arrayMaxConsecutiveSum(inputArray, k) {
+    let sum = 0;
+    let max = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += inputArray[i];     
+    }
+
+    max = sum;
+
+    for (let i = k; i < inputArray.length; i++) {
+        sum -= inputArray[i-k];
+        sum += inputArray[i];   
+        
+        if (sum > max) {
+            max = sum;
+        }
+    }
+
+    return max;
+}
+
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
